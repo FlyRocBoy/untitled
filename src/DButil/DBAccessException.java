cfcafa0a75c81d0830b312b86c669455b5ebfa44
gaopeng <467516920@qq.com>
2017-10-09 14:22:49 +0800

    push

---
+++ src/DButil/DBAccessException.java
@@ -0,0 +1,40 @@
+
+package DButil;
+
+public class DBAccessException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	protected Throwable throwable;
+
+	/**
+	 * 构造方法
+	 * 
+	 * @param message
+	 */
+	public DBAccessException(String message) {
+		super(message);
+
+	}
+
+	/**
+	 * 构造方法
+	 * 
+	 * @param message
+	 * @param throwable
+	 */
+	public DBAccessException(String message, Throwable throwable) {
+		super(message);
+		this.throwable = throwable;
+	}
+
+	/**
+	 * 返回底层异常原因
+	 * 
+	 * @return Throwable
+	 */
+	public Throwable getCause() {
+		return throwable;
+	}
+
+}
--- src/ExcelToBean/Test.java
+++ src/ExcelToBean/Test.java
@@ -1,13 +1,25 @@
 package ExcelToBean;
 
+import DButil.ConnectionManager;
+import DButil.DBAccessException;
+
 import java.io.File;
 import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Test
 {
+	private Connection connection;
+
+	private PreparedStatement titlesQuery;
+
+	private ResultSet results;
 
 	public static void main(String[] args) throws Exception
 	{
@@ -30,6 +42,44 @@ public class Test
 		{
 			System.out.println(export.getError().toString());
 		}
+
 	}
 
+	@org.junit.Test
+public void connTest() {
+		try {
+			try {
+			 connection = ConnectionManager.getConnection();
+			} catch (DBAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			titlesQuery = connection
+					.prepareStatement("select * from user_info limit 10");
+			ResultSet results = titlesQuery.executeQuery();
+
+			// 读取行数据
+			while (results.next()) {
+				System.out.println(results.getString("user_id"));
+			}
+		}
+
+		// 处理数据库异常
+		catch (SQLException exception) {
+			exception.printStackTrace();
+		}
+
+		// 释放资源
+		finally {
+			ConnectionManager.closeResultSet(results);
+			ConnectionManager.closeStatement(titlesQuery);
+			ConnectionManager.closeConnection(connection);
+		}
+
+	}
+
+
 }
+
+
+
